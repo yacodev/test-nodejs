89dd03bed92f62e7d7d7a166785b89085d2ff348
Carlos yaco <cyaco33@gmail.com>
2022-11-07 22:00:43 -0500

    delete cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: true }));
+//app.use(cors({ origin: true }));
 
 app.get("/tokenizeOrder", (req, res) => {
   console.log("procesando...");
